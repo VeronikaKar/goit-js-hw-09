81934d25155be4e0234212dc3c2331df2ea455e6
VeronikaKar <157287209+VeronikaKar@users.noreply.github.com>
2024-04-03 12:51:51 +0000

    Deploying to gh-pages from @ VeronikaKar/goit-js-hw-09@42e811a03333c36dcdfb306231c5fd79324f6a0c 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{S as a}from"./assets/vendor-870f0eb5.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=commonHelpers.js.map
