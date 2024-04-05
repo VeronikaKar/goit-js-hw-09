3308848252ecb30c13173e57c6c0d39fa36bf4fa
VeronikaKaraul <veronika.life@gmail.com>
2024-04-05 20:18:06 +0300

    Update 2
    includes event-  destructurization+reset

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,13 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('.feedback-form');
-  const emailInput = form.querySelector('input[name="email"]');
-  const messageInput = form.querySelector('textarea[name="message"]');
-  const submitButton = form.querySelector('button[type="submit"]');
 
-  function saveFormData() {
+  const form = document.querySelector('.feedback-form');
+ form.addEventListener('input', function (event) {
+   const { email, message } = event.currentTarget.elements;
+   saveFormData(email, message);
+  });
+
+
+  function saveFormData(a, b) {
     const formData = {
-      email: emailInput.value.trim(),
-      message: messageInput.value.trim()
+      email: a.value.trim(),
+      message: b.value.trim()
     };
 
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
@@ -15,35 +17,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const savedFormData = JSON.parse(localStorage.getItem('feedback-form-state'));
   if (savedFormData) {
-    emailInput.value = savedFormData.email;
-    messageInput.value = savedFormData.message;
+    form.elements.email.value = savedFormData.email;
+    form.elements.message.value = savedFormData.message;
   }
 
-  form.addEventListener('input', function () {
-    saveFormData();
-  });
-
+ 
   form.addEventListener('submit', function (event) {
     event.preventDefault();
-
+   const { email, message } = event.currentTarget.elements;
     const formData = {
-      email: emailInput.value.trim(),
-      message: messageInput.value.trim()
+      email: email.value.trim(),
+      message: message.value.trim()
     };
 
-    if (!emailInput.value () || !messageInput.value ()) {
+    if (!email.value.trim () || !message.value.trim ()) {
       alert('"Local storage is empty!".');
       return;
     }
 
-    localStorage.removeItem('feedback-form-state');
-    emailInput.value = '';
-    messageInput.value = '';
+ console.log('Submitted form data:', formData);
 
- 
-    console.log('Submitted form data:', formData);
+    localStorage.removeItem('feedback-form-state');
+    event.currentTarget.reset();
   });
-});
 // Зайві операції Trim: У прослуховувачі події submit об'єкт даних форми (formData) створюється після очищення полів форми. Це призводить до того, що обидві властивості, email та message, будуть порожніми рядками, незалежно від застосованого trim.
 // Критичні помилки:
 
